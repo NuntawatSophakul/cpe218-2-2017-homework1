02b7bcfe247dbbf84ab35b2f1a0528320ee07cba
NuntawatSophakul <31124804+NuntawatSophakul@users.noreply.github.com>
2018-01-21 23:04:06 +0700

    Update Homework1.java

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,16 +1,67 @@
 
-public class Homework1 {
 
+
+import java.util.Stack;
+
+
+public class Homework1 {
+	public static  Node tree ;
+	public static Stack<Character> Cat =new Stack<Character>();
 	public static void main(String[] args) {
-		// Begin of arguments input sample
-		if (args.length > 0) {
-			String input = args[0];
-			if (input.equalsIgnoreCase("251-*32*+")) {
-				System.out.println("(2*(5-1))+(3*2)=14");
-			}
+
+		//String input = "251-*32*+";
+
+		String posfix = args[0];
+		for(int i=0;i<posfix.length();i++){
+			Cat.add(posfix.charAt(i));
 		}
-		// End of arguments input sample
-		
-		// TODO: Implement your project here
+		tree = new Node(Cat.pop());
+		Infix(tree);
+		iNorder(tree);
+		System.out.print("="+Calculate(tree));
+
 	}
+	public static void Infix(Node Nuntawat){
+		if(Nuntawat.Aun == '+' || Nuntawat.Aun == '-' || Nuntawat.Aun == '*' || Nuntawat.Aun == '/'){
+			Nuntawat.right = new Node(Cat.pop());
+			Infix(Nuntawat.right);
+			Nuntawat.left = new Node(Cat.pop());
+			Infix(Nuntawat.left);
+		}
+	}
+	public static int Calculate(Node Alcohol){
+		if(Alcohol.Aun == '+')
+		{
+			return Calculate(Alcohol.left)+Calculate(Alcohol.right);
+		}
+		else  if(Alcohol.Aun == '-')
+		{
+			return Calculate(Alcohol.left)-Calculate(Alcohol.right);
+		}
+		else  if(Alcohol.Aun == '*')
+		{
+			return Calculate(Alcohol.left)*Calculate(Alcohol.right);
+		}
+		else  if(Alcohol.Aun == '/')
+		{
+			return  Calculate(Alcohol.left)/Calculate(Alcohol.right);
+		}
+		else return Integer.parseInt(Alcohol.Aun.toString());
+	}
+
+	public static void iNorder(Node Beer) {
+		if(Beer.Aun == '+' || Beer.Aun == '-' || Beer.Aun == '*' || Beer.Aun == '/'){
+			if(Beer!=tree)System.out.print("(");
+			iNorder(Beer.left);
+			System.out.print(Beer.Aun);
+			iNorder(Beer.right);
+			if(Beer!=tree)System.out.print(")");
+		}else
+		{
+			System.out.print(Beer.Aun);
+		}
+	}
+
+
 }
+//upไม่ได้

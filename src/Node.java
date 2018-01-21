443a2c7b4f0d9e135c6c7ccaced36b5feadcbf41
NuntawatSophakul <31124804+NuntawatSophakul@users.noreply.github.com>
2018-01-21 23:04:55 +0700

    55+

---
+++ src/Node.java
@@ -0,0 +1,11 @@
+import jdk.nashorn.internal.runtime.regexp.joni.encoding.CharacterType;
+
+public class Node {
+    public Node left;
+    public  Node right;
+    Character Aun ;
+    public Node (char input)
+    {
+        Aun = input;
+    }
+}
